f9c1bc8e679fd579f28cb0f6c2e990ea1b6deaff
Merajul Islam <meraj.exe@gmail.com>
2023-04-10 20:34:00 +0600

    header finished

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import './Home.css';
+import Header from '../Header/Header';
 const Home = () => {
   return (
-    <div className="bg-home">
-      <div className="home">
-        <div className="home-description">
-          <h1>
-            One Step Closer To Your <span className="dream-job">Dream Job</span>
-          </h1>
-          <p>
-            Explore thousands of job opportunities with all the information you
-            need. Its your future. Come find it. Manage all your job application
-            from start to finish.
-          </p>
-          <button className="btn-common">Get Started</button>
-        </div>
-        <img src="../../assets/All Images/P3OLGJ1 copy 1.png" alt="" />
-      </div>
+    <div>
+      <Header></Header>
     </div>
   );
 };
