4d9a253b64ff8f3b91c2cd058de07076103225b2
Merajul Islam <meraj.exe@gmail.com>
2023-04-11 00:36:22 +0600

    featured job start

---
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FeaturedJobs = () => {
+  return (
+    <div>
+      <h2>jobs</h2>
+    </div>
+  );
+};
+
+export default FeaturedJobs;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 import Header from '../Header/Header';
 import Categories from '../Categories/Categories';
+import { useLoaderData } from 'react-router-dom';
+import FeaturedJobs from '../FeaturedJobs/FeaturedJobs';
 const Home = () => {
+  const jobsInitial = useLoaderData();
+  const [jobs, setJobs] = useState(jobsInitial.slice(0, 4));
+  console.log(jobs);
+  const showAllJob = () => {
+    setJobs(jobsInitial);
+  };
   return (
     <div>
       <Header></Header>
       <Categories></Categories>
+      <div className="featured-jobs">
+        {jobs.map(job => (
+          <FeaturedJobs key={job.id}></FeaturedJobs>
+        ))}
+      </div>
+      <button onClick={showAllJob}>See All Jobs</button>
     </div>
   );
 };
