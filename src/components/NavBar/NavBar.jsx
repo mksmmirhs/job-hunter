4a3e3cc52df597fe35d40c607bdf3c14dadcce3d
Merajul Islam <meraj.exe@gmail.com>
2023-04-10 20:19:28 +0600

    navbar finished

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './NavBar.css';
+import { Link } from 'react-router-dom';
+const NavBar = () => {
+  return (
+    <div className="bg-header">
+      <nav>
+        <div className="logo">JobHunter</div>
+        <div className="links">
+          <Link to="statistics">Statistics</Link>
+          <Link to="apply">Applied Jobs</Link>
+          <Link to="blog">Blog</Link>
+        </div>
+        <div>
+          <button className="btn-common">Star Applying</button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import './index.css';
+import Layout from './components/Layout/Layout';
+import ErrorPage from './error-page';
+import Home from './components/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout></Layout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
